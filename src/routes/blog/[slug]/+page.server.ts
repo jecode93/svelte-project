a67524bbfaf60b41a0bd93ac937f81d818e41134
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-06-08 20:08:15 -0400

    refactor: use the $lib alias to import the posts data from the data file.

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
 import { error } from '@sveltejs/kit';
-import { posts } from '../data';
+import { posts } from '$lib/data';
 
 export function load({ params }) {
 	const post = posts.find((post) => post.slug === params.slug);
